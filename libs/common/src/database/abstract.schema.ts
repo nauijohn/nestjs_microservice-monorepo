c9ecc4b23942694a4faffd2560187b843b91057f
alctech-nauijohn <85679013+nauijohn@users.noreply.github.com>
2025-02-02 12:11:16 +0800

    abstract repository init

---
+++ libs/common/src/database/abstract.schema.ts
@@ -0,0 +1,9 @@
+import { SchemaTypes, Types } from 'mongoose';
+
+import { Prop, Schema } from '@nestjs/mongoose';
+
+@Schema()
+export class AbstractDocument {
+  @Prop({ type: SchemaTypes.ObjectId })
+  _id: Types.ObjectId;
+}
